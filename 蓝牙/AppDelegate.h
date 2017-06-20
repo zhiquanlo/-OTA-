0b10380af0ea98f11b7d6f29c295d9b8dea8ceaf
李志权 <lizhiquan@lizhiquandeiMac.local>
2017-06-20 16:42:25 +0800

    Initial Commit

---
+++ 蓝牙/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  蓝牙
+//
+//  Created by 李志权 on 2017/6/20.
+//  Copyright © 2017年 李志权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 蓝牙/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  蓝牙
+//
+//  Created by 李志权 on 2017/6/20.
+//  Copyright © 2017年 李志权. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
